922aa27e0274447e236700f1a5bc0fa937c001dd
ChanduGavini <webdev.chandu77@gmail.com>
2024-05-28 17:09:46 +0530

    Alert Added

---
+++ Alert/App.js
@@ -0,0 +1,20 @@
+import { View, Alert, Button } from 'react-native'
+
+export default function App () {
+  return(
+    <View style={{ flex: 1, backgroundColor: 'plum', padding: 60}}>
+      <Button title='Alert' onPress={() => Alert.alert('Invalid Data!')}/>
+      <Button title='Alert 2' onPress={() => Alert.alert('Invalid Data!', "DOB Incorrect")}/>
+      <Button title='Alert 3' onPress={() => Alert.alert('Invalid Data!', "DOB Incorrect", [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed')
+        },
+        {
+          text: 'Ok',
+          onPress: () => console.log('Ok Pressed')
+        }
+      ])}/>
+    </View>
+  )
+}
